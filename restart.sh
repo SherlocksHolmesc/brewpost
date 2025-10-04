894307b2d0f150455db75f3e3a3148ab10b8d385
SherlocksHolmesc <144738101+SherlocksHolmesc@users.noreply.github.com>
2025-10-05 00:37:49 +0800

    sd

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pkill -f unified-server || true
-cd /home/brewpost/brewpost
+cd /home/brewpost/brewpost/server
 aws secretsmanager get-secret-value --secret-id env --query SecretString --output text | jq -r 'to_entries[] | "\(.key)=\(.value)"' > .env
 echo "Environment variables loaded:"
 cat .env
