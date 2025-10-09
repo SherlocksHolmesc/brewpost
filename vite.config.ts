8189869ec5d1f9437b35813b39be83d02704e2bb
SherlocksHolmesc <144738101+SherlocksHolmesc@users.noreply.github.com>
2025-10-09 16:31:53 +0800

    tai

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,12 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/generate': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path, // keep path /generate
       },
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
